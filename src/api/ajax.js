f990cf74ae20962d02c614574846a1575e03077c
jacki.heo <38304972+Edenpartners@users.noreply.github.com>
2019-04-27 19:48:46 +0900

    API EP modified

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -12,8 +12,7 @@ export const axInstance = axios.create({
 });
 
 // default api path
-//axInstance.defaults.baseURL  = "https://eexplorer-api.edenchain.io/api"
-axInstance.defaults.baseURL  = "http://192.168.239.91:8081/api"
+axInstance.defaults.baseURL  = "https://eexplorer-api.edenchain.io/api"
 
 // for convinient, response is modified.
 axInstance.interceptors.response.use((response => {
